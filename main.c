bb24dfeac37e919dcf912b0571831ab6931ca5c5
Nestor Huallpa <huallpa.nestor@gmail.com>
2017-03-22 02:00:48 -0300

    version inicial

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+*   6620 - Organizacion del computador 
+*   Trabajo Practico 0 
+*   Alumnos: 
+*           88614 - Nestor Huallpa
+*           88573 - Ariel Martinez
+*           xxxxx - Pablo Sivori
+*/
+
+#include <stdio.h>
+
+static const char basis_64[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+
+int main(int argc, char** argv) {
+
+
+   
+
+  return 0;
+}
+
