bd36f6c7ca716f1ef7facadabc42d6ae67fd2771
Nestor Huallpa <huallpa.nestor@gmail.com>
2017-06-18 01:57:07 -0300

    Corrijo casos e informe

--- tp1/pruebas/pruebas.sh
+++ tp1/pruebas/pruebas.sh
@@ -2,37 +2,42 @@
 echo "==========================COMIENZO PRUEBAS==============================="
 echo "=============================TEST 1======================================"
 echo "Test 1: Codificamos Man ingresando por stdin y devolviendo por stdout"
-echo Man | ../tp1 -a encode
+echo Man | ./tp1 -a encode
 echo ""
 echo "============================FIN TEST1===================================="
 echo "========================================================================="
 echo "=============================TEST 2======================================"
 echo "Test 2: Codificamos y decodificamos Man, entrada stdin y salida stdout"
-echo -n Man | ../tp1 | ../tp1 -a decode
+echo -n Man | ./tp1 | ./tp1 -a decode | od -t c
 echo ""
 echo "============================FIN TEST2===================================="
 echo "========================================================================="
 echo "=============================TEST 3======================================"
 echo "Test 3: Verificamos bit a bit"
-echo xyz | ../tp1 | ../tp1 -a decode | od -t c
+echo xyz | ./tp1 | ./tp1 -a decode | od -t c
 echo "============================FIN TEST3===================================="
 echo "========================================================================="
 echo "=============================TEST 4======================================"
 echo "Test 4: Codificamos 1024 bytes y que no haya mas de 76 unidades de long."
-      yes | head -c 1024 | ../tp1 -a encode
+      yes | head -c 1024 | ./tp1 -a encode | od -t c
 echo ""
 echo "============================FIN TEST4===================================="
 echo "========================================================================="
 echo "=============================TEST 5======================================"
 echo "Test 5: Verificamos que la cantidad de bytes decodificados, sea 1024."
-      yes | head -c 1024 | ../tp1 -a encode | ../tp1 -a decode | wc -c
+      export ORI=archivo_orig_yes.txt &&
+      export RES=archivo_result_yes.txt &&
+      yes | head -c 1024 | tee $ORI | ./tp1 | ./tp1 -a decode | tee $RES | wc -c && 
+      diff -s $ORI $RES && 
+      cat $RES | od -t c
 echo "============================FIN TEST5===================================="
 echo "========================================================================="
 echo "=============================TEST 6======================================"
 echo "Test 6: Codifico el contenido del archivo de entrada y guardo en archivo"
 echo "        de salida. Luego decodifico la salida de este archivo y lo mando"
 echo "        a otro archivo de entrada."
-../tp1 -i entrada.txt -o salida.txt -a encode
+../tp1 -i entrada.txt -o salida.txt -a encode 
 ../tp1 -i salida.txt -o entrada2.txt -a decode
+diff -s entrada.txt entrada2.txt 
 echo "============================FIN TEST6===================================="
 echo "===========================FIN PRUEBAS==================================="
